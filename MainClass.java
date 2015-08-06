365fb5fdc7410a0161e08b79ea7472cb451ca41c
Soumik <soumik.shah@gmail.com>
2015-08-06 16:20:49 -0700

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class File1Reader {
-	File3Reader table;
+	FileReader table;
 	int counter = 1;
 
 	public void run(File f) throws IOException {
